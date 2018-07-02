162c0ccf3023d62b45e0c639e070c96157ec27a1
Gabor Janvari (DELL) <janvari.gabor2@gmail.com>
2018-07-02 16:39:44 +0200

    Dátum gyakorlás

--- src/Datum_kezeles_proba/Aktualis_datum_lekerdezes.java
+++ src/Datum_kezeles_proba/Aktualis_datum_lekerdezes.java
@@ -1,7 +1,10 @@
 package Datum_kezeles_proba;
 
 import java.text.DateFormat;
+import java.time.Instant;
+import java.time.Month;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 
 public class Aktualis_datum_lekerdezes {
@@ -12,7 +15,10 @@ public class Aktualis_datum_lekerdezes {
 	   String ev, honapSzoveg;
 	   int honap=0,nap=0;
 	   String h="",n="";
-
+	   long elteltido;
+	   long szulinap;
+	   long ma;
+	   long ev_,ho_, nap_;
 	   System.out.println("Dátum és idõ formátum LONG, magyar: "+df.format(c.getTime()));
 	   
 	   df=DateFormat.getDateTimeInstance(DateFormat.MEDIUM,DateFormat.MEDIUM,Locale.getDefault());
@@ -23,6 +29,19 @@ public class Aktualis_datum_lekerdezes {
 	   
 	   df=DateFormat.getDateTimeInstance(DateFormat.SHORT,DateFormat.SHORT,Locale.ENGLISH);
 	   System.out.println("Dátum és idõ formátum SHORT, angol: "+df.format(c.getTime())); 
+	   
+	   Calendar gszulinap = new GregorianCalendar(1975,Calendar.JUNE,23);
+	   String s = String.format("Gábor's Birthday: %1$tm %1$te,%1$tY", gszulinap);
+	   System.out.println(s);
+	   szulinap=gszulinap.getTimeInMillis();
+	   ma=c.getTimeInMillis();
+	   elteltido=ma-szulinap;
+	   Calendar k = Calendar.getInstance();
+	   k.set(0,0,0,0,0,0);
+	   //k.set(Calendar.MILLISECOND, 0);
+	   ev_=k.getTimeInMillis();
+	   System.out.println(Instant.ofEpochMilli(ev_).toString());
+	   
 	   
 	   /*
    	ev=c.get(Calendar.YEAR)+"";
