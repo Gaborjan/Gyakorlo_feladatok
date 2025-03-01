8e2774b89d7542135ea85e3094f8e1b37594439f
Jánvári Gábor (Asus) <janvari.gabor2@gmail.com>
2025-03-01 14:33:04 +0100

    KALÓRIA

--- src/iteracios_feladatok_14_fejezet/Kaloria_14_4.java
+++ src/iteracios_feladatok_14_fejezet/Kaloria_14_4.java
@@ -1,28 +1,29 @@
 package iteracios_feladatok_14_fejezet;
 
-/*Kérjük be a héten mennyi kalóriát fogyasztottunk az egyes napokon! Ezután írjuk ki az összes
- * kalóriafogyasztásunkat, valamint a napi átlag fogyasztást! 
+/*Kï¿½rjï¿½k be a hï¿½ten mennyi kalï¿½riï¿½t fogyasztottunk az egyes napokon! Ezutï¿½n ï¿½rjuk ki az ï¿½sszes
+ * kalï¿½riafogyasztï¿½sunkat, valamint a napi ï¿½tlag fogyasztï¿½st! 
  */
 import extra.*;
+
 public class Kaloria_14_4 {
 	public static void main(String[] args) {
 		final int MINKAL=0;
 		final int MAXKAL=5000;
 		int napi=0, heti=0;
 		double atlag=0;
-		System.out.println("A program bekéri a napi kalóriafogyasztást, majd a végén kiírja a mennyi kalóriát");
-		System.out.println("fogyasztottunk a héten összesen, és ez milyen napi kalóriabevitelt jelent átlagosan.");
-		System.out.println("Csak egész értékû kalóriát vigyen be, min. 0, max. 5000 kcal/nap.");
+		System.out.println("A program bekï¿½ri a napi kalï¿½riafogyasztï¿½st, majd a vï¿½gï¿½n kiï¿½rja a mennyi kalï¿½riï¿½t");
+		System.out.println("fogyasztottunk a hï¿½ten ï¿½sszesen, ï¿½s ez milyen napi kalï¿½riabevitelt jelent ï¿½tlagosan.");
+		System.out.println("Csak egï¿½sz ï¿½rtï¿½kï¿½ kalï¿½riï¿½t vigyen be, min. 0, max. 5000 kcal/nap.");
 		for (int i=1;i<=7;i++) { 
 			do
-				napi=Console.readInt("Kérem a "+i+". napi kalóriát: ");
+				napi=Console.readInt("Kï¿½rem a "+i+". napi kalï¿½riï¿½t: ");
 			while (napi<MINKAL || napi>MAXKAL);
 			heti+=napi;
 		}
 		atlag=heti/7;
-		System.out.printf("Heti kalóriabevitel: %10d kcal \n",heti);
-		System.out.printf("Napi átlag         : %,10.2f kcal \n",atlag);
-		System.out.println("Program vége:");
+		System.out.printf("Heti kalï¿½riabevitel: %10d kcal \n",heti);
+		System.out.printf("Napi ï¿½tlag         : %,10.2f kcal \n",atlag);
+		System.out.println("Program vï¿½ge:");
 		Console.pressEnter();
 	}
 
